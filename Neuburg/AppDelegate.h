9335c357c128a3a94739d95ed91416f0319c4058
Mario Abruzzi <abruzzim@framework-solutions.com>
2015-03-16 20:59:12 -0400

    Initial Commit

---
+++ Neuburg/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Neuburg
+//
+//  Created by abruzzim on 3/16/15.
+//  Copyright (c) 2015 Mario Abruzzi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
