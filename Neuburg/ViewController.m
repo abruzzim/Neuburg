b833c2e15bbae0b9a75621c6a1311049b5290c9c
Mario Abruzzi <abruzzim@framework-solutions.com>
2015-03-16 21:58:07 -0400

    Demo Frames and Views

--- Neuburg/ViewController.m
+++ Neuburg/ViewController.m
@@ -17,6 +17,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIView *mainView = self.view;
+    mainView.backgroundColor = [UIColor grayColor];
+    
+    UIView *view1 = [[UIView alloc] initWithFrame:CGRectMake(113, 111, 132, 194)];
+    view1.backgroundColor = [UIColor colorWithRed:1.0 green:0.4 blue:1.0 alpha:1.0];
+    
+    UIView *view2 = [[UIView alloc] initWithFrame:CGRectMake(41, 56, 132, 194)];
+    view2.backgroundColor = [UIColor colorWithRed:0.5 green:1.0 blue:0.0 alpha:1.0];
+    
+    UIView *view3 = [[UIView alloc] initWithFrame:CGRectMake(43, 197, 160, 230)];
+    view3.backgroundColor = [UIColor colorWithRed:1.0 green:0.0 blue:0.0 alpha:1.0];
+    
+    [mainView addSubview:view1];
+    [view1 addSubview:view2];
+    [mainView addSubview:view3];
 }
 
 - (void)didReceiveMemoryWarning {
