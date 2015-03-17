873db291670ff2fc122d13af5524e0cdef7ed660
Mario Abruzzi <abruzzim@framework-solutions.com>
2015-03-17 01:41:19 -0400

    Demo More Transforms

--- Neuburg/ViewController.m
+++ Neuburg/ViewController.m
@@ -41,6 +41,18 @@
     UIView *view7 = [[UIView alloc] initWithFrame:CGRectInset(view6.bounds, 10, 10)]; // Refers to view6's bounds.
     view7.backgroundColor = [UIColor colorWithRed:1.0 green:0.1 blue:1.0 alpha:0.9];
     
+    UIView *view8 = [[UIView alloc] initWithFrame:CGRectMake(10, 350, 132, 194)];
+    view8.backgroundColor = [UIColor colorWithRed:0.0 green:0.5 blue:0.2 alpha:1.0];
+    
+    UIView *view9 = [[UIView alloc] initWithFrame:CGRectInset(view8.bounds, 10, 10)]; // Refers to view8's bounds.
+    view9.backgroundColor = [UIColor colorWithRed:1.0 green:0.1 blue:1.0 alpha:0.9];
+    
+    UIView *view10 = [[UIView alloc] initWithFrame:CGRectMake(150, 450, 132, 194)];
+    view10.backgroundColor = [UIColor colorWithRed:0.0 green:0.5 blue:0.2 alpha:1.0];
+    
+    UIView *view11 = [[UIView alloc] initWithFrame:CGRectInset(view10.bounds, 10, 10)]; // Refers to view10's bounds.
+    view11.backgroundColor = [UIColor colorWithRed:1.0 green:0.1 blue:1.0 alpha:0.9];
+    
     [mainView addSubview:view1];
     [view1 addSubview:view2];
     [mainView addSubview:view3];
@@ -48,6 +60,10 @@
     [view4 addSubview:view5];
     [mainView addSubview:view6];
     [view6 addSubview:view7];
+    [mainView addSubview:view8];
+    [view8 addSubview:view9];
+    [mainView addSubview:view10];
+    [view10 addSubview:view11];
     
     CGRect r4 = view4.bounds; // Get superview's frame.
     r4.origin.x += 25;        // Set superview's x-origin.
@@ -60,6 +76,10 @@
     view5.bounds = r5;        // Change the inset view's frame.
     
     view6.transform = CGAffineTransformMakeRotation(45 * M_PI/180.0); // Rotation transform.
+    
+    view8.transform = CGAffineTransformMakeScale(0.8, 1.0); // Scale transform.
+    
+    view10.transform = CGAffineTransformMake(1, 0, -0.2, 1, 0, 0); // Skew transform.
 }
 
 - (void)didReceiveMemoryWarning {
